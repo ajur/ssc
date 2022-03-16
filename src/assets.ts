53af1fb8dc74e4528d009cbfc79c73e838802942
Adam Jurczyk <ajur.pl@gmail.com>
2022-03-16 02:00:51 +0100

    fix assets paths

--- src/assets.ts
+++ src/assets.ts
@@ -1,9 +1,15 @@
 import { SoundSpriteDataMap } from "@pixi/sound";
 import { IAddOptions, LoaderResource } from "pixi.js";
 
-const withSprites = 
-    (sprites: SoundSpriteDataMap) => 
-        (resource: LoaderResource) => resource.sound?.addSprites(sprites);
+export function assets(): IAddOptions[] {
+    return [
+        { url: "/simon/TeacherA.ttf", name: "TeacherA"},
+        { url: "/simon/AegrisOutlineRegular.ttf", name: "AegisOutlet"},
+        { url: "/simon/simon.json" },
+        { url: "/simon/music.mp3", name: "simon_music"},
+        { url: "/simon/notes.mp3", name: "simon_notes", onComplete: withSprites(simonNotesSprite) }
+    ].map(fixBasePath);
+}
 
 const simonNotesSprite: SoundSpriteDataMap = {
     'note-0': { start: 0, end: 1 },
@@ -14,12 +20,14 @@ const simonNotesSprite: SoundSpriteDataMap = {
     'note-5': { start: 5, end: 6 },
 }
 
-export function assets(): IAddOptions[] {
-    return [
-        { url: "/simon/TeacherA.ttf", name: "TeacherA"},
-        { url: "/simon/AegrisOutlineRegular.ttf", name: "AegisOutlet"},
-        { url: "/simon/simon.json" },
-        { url: "/simon/music.mp3", name: "simon_music"},
-        { url: "/simon/notes.mp3", name: "simon_notes", onComplete: withSprites(simonNotesSprite) }
-    ];
-}
+const withSprites = 
+    (sprites: SoundSpriteDataMap) => 
+        (resource: LoaderResource) => resource.sound?.addSprites(sprites);
+
+const fixBasePath = 
+    (opt: IAddOptions) => {
+        if (opt.url?.startsWith('/')) {
+            return {...opt, url: import.meta.env.BASE_URL + opt.url.substring(1)};
+        }
+        return opt;
+    };
